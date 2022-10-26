fcb6e2181c075a7c8c38267e79c89865ee691e49
Sant <49729387+Vsants@users.noreply.github.com>
2022-10-25 23:02:32 -0300

    Painel finalizado (#2)

--- Painel.py
+++ Painel.py
@@ -3,12 +3,20 @@ from PySimpleGUI import PySimpleGUI as sg;
 #Layout
 sg.theme('Reddit')
 layout = [
-    [sg.Text('Usuário'), sg.Input(key='usuario')],
-    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Text('Usuário'), sg.Input(key='usuario', size=(25,1))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*',  size=(25,1))],
     [sg.Checkbox('Lembrar login')],
     [sg.Button('Entrar')]
 ]
 
 #Window
+janela = sg.Window('Inicie uma sessão', layout)
 
 #Event
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'User' and valores ['senha'] == 'Password@123456':
+            print('Bem-vindo! Você iniciou uma nova sessão.')
